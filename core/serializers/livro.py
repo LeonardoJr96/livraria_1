a2297303bf8aa9adf33f84aeab9fdce3cd80696f
Leonardo Junior <leo70x7000@gmail.com>
2024-12-23 15:10:22 -0300

    Upload e associação de imagens

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,9 +1,25 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 from core.models import Categoria, Livro
 
         
 class LivroSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source="capa",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(
+        required=False,
+        read_only=True
+    )
+    
     class Meta:
         model = Livro
         fields = "__all__"
@@ -14,6 +30,9 @@ class LivroListSerializer(ModelSerializer):
         fields = ("id", "titulo", "preco")
 
 class LivroRetrieveSerializer(ModelSerializer):
+    
+    capa = ImageSerializer(required=False)
+    
     class Meta:
         model = Livro
         fields = "__all__"
