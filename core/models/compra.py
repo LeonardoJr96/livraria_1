95852ee249761820b25e0503fb2ad36086d831ad
Leonardo Junior <leo70x7000@gmail.com>
2024-12-23 16:42:59 -0300

    Acrescentando a data da compra

--- core/models/compra.py
+++ core/models/compra.py
@@ -12,7 +12,8 @@ class Compra(models.Model):
 
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
     status = models.IntegerField(choices=StatusCompra.choices,  default=StatusCompra.CARRINHO) 
-
+    data = models.DateTimeField(auto_now_add=True)
+    
 class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
     livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
