0da1b441ae92102d3b3019fc7d1efb4217ff6f4e
Leonardo de Almeida Junior <leo70x7000@gmail.com>
2025-02-13 09:02:27 -0300

    listagem dos 10 livros mais vendidos

--- core/views/livro.py
+++ core/views/livro.py
@@ -19,32 +19,47 @@ class LivroViewSet(ModelViewSet):
             return LivroRetrieveSerializer
         return LivroSerializer
 
-        @action(detail=True, methods=["patch"])
-        def alterar_preco(self, request, pk=None):
-            livro = self.get_object()
+    @action(detail=True, methods=["patch"])
+    def alterar_preco(self, request, pk=None):
+        livro = self.get_object()
 
-            serializer = LivroAlterarPrecoSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
+        serializer = LivroAlterarPrecoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-            livro.preco = serializer.validated_data["preco"]
-            livro.save()
+        livro.preco = serializer.validated_data["preco"]
+        livro.save()
 
-            return Response(
-                {"detail": f"Preço do livro '{livro.titulo}' atualizado para {livro.preco}."}, status=status.HTTP_200_OK
-            )
+        return Response(
+            {"detail": f"Preço do livro '{livro.titulo}' atualizado para {livro.preco}."}, status=status.HTTP_200_OK
+        )
         
-        @action(detail=True, methods=["post"])
-        def ajustar_estoque(self, request, pk=None):
-            livro = self.get_object()
+    @action(detail=True, methods=["post"])
+    def ajustar_estoque(self, request, pk=None):
+        livro = self.get_object()
 
-            serializer = LivroAjustarEstoqueSerializer(data=request.data, context={"livro": livro})
-            serializer.is_valid(raise_exception=True)
+        serializer = LivroAjustarEstoqueSerializer(data=request.data, context={"livro": livro})
+        serializer.is_valid(raise_exception=True)
 
-            quantidade_ajuste = serializer.validated_data["quantidade"]
+        quantidade_ajuste = serializer.validated_data["quantidade"]
 
-            livro.quantidade += quantidade_ajuste
-            livro.save()
+        livro.quantidade += quantidade_ajuste
+        livro.save()
 
-            return Response(
-                {"status": "Quantidade ajustada com sucesso", "novo_estoque": livro.quantidade}, status=status.HTTP_200_OK
-            )
+        return Response(
+            {"status": "Quantidade ajustada com sucesso", "novo_estoque": livro.quantidade}, status=status.HTTP_200_OK
+        )
+            
+    @action(detail=False, methods=["get"])
+    def mais_vendidos(self, request):
+        livros = Livro.objects.annotate(total_vendidos=Sum("itenscompra__quantidade")).filter(total_vendidos__gt=10)
+
+        data = [
+            {
+                "id": livro.id,
+                "titulo": livro.titulo,
+                "total_vendidos": livro.total_vendidos,
+            }
+            for livro in livros
+        ]
+
+        return Response(data, status=status.HTTP_200_OK)
